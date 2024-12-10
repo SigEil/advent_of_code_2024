c1469314c582de1c1f3b8a63949b1779ed51a7ed
SigEil <60142388+SigEil@users.noreply.github.com>
2024-12-10 16:48:18 +0100

    Improved maths logic and removed list conversion

--- day_1.py
+++ day_1.py
@@ -1,21 +1,11 @@
-# Global difference counter
-difference = 0
-
-# Create lists from input to do operations
-with open ('Assets/day_1_input.txt') as file:
+# Split file into tuples then sort
+with open('Assets/day_1_input.txt') as file:
     array1, array2 = zip(*(map(int, line.split()) for line in file))
 
-array1 = list(array1)
-array2 = list(array2)
+array1 = tuple(sorted(array1))
+array2 = tuple(sorted(array2))
 
-array1.sort()
-array2.sort()
+# Calculate the total difference
+difference = sum(abs(a - b) for a, b in zip(array1, array2))
 
-# Add the differences
-for i in range(len(array1)):
-    if array1[i] > array2[i]:
-        difference += (array1[i] - array2[i])
-    else:
-        difference += (array2[i] - array1[i])
-
-print('Difference: ', difference)
+print('Difference:', difference)
